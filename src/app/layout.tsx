dd67e5f0e2a4db0336030fc4d838538cc288a2f8
rafaellore <birdneon3@gmail.com>
2024-06-19 11:53:18 -0300

    feat(Header): adds initial header configs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { type_second } from "@/functions/fonts";
+import Header from "@/components/Header/header";
 
 export const metadata: Metadata = {
   title: "Dogs NextJS",
@@ -15,7 +16,10 @@ export default function RootLayout({
 }>) {
   return (
     <html className={type_second.variable} lang="pt-BR">
-      <body>{children}</body>
+      <body>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
