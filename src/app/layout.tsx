d89b21dfcf889945a0554b40af622c03deb64861
rafaellore <birdneon3@gmail.com>
2024-06-25 15:28:33 -0300

    feat(userContext): adds user data context
    to prevent the fetch, and runs only a single time

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import "./globals.css";
 import { type_second } from "@/functions/fonts";
 import Header from "@/components/Header/header";
 import Footer from "@/components/Footer/footer";
+import userGet from "@/actions/userGet";
+import { UserContextProvider } from "@/context/userContext";
 
 export const metadata: Metadata = {
   title: "Dogs NextJS",
@@ -14,15 +16,18 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const { data: user } = await userGet();
+
   return (
     <html lang="pt-BR">
       <body className={type_second.variable}>
-        <div className="App">
-          <Header />
-          <main className="AppBody">{children}</main>
-          <Footer />
-        </div>
+        <UserContextProvider user={user}>
+          <div className="App">
+            <Header />
+            <main className="AppBody">{children}</main>
+            <Footer />
+          </div>
+        </UserContextProvider>
       </body>
     </html>
-  );
 }
