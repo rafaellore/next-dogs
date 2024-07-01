29004d670e0cd6d40f15a84558c028b116f9205d
rafaellore <birdneon3@gmail.com>
2024-07-01 18:21:46 -0300

    feat(photoPost): adds photo post form + action

--- src/app/conta/postar/page.tsx
+++ src/app/conta/postar/page.tsx
@@ -1,5 +1,12 @@
-export default async function ContaPostarPage() {
-  return (
-    <div>ContaPage</div>
-  )
+import ContaPhotoPost from '@/components/conta/ContaPhotoPost';
+import { Metadata } from 'next';
+
+export const runtime = 'edge';
+
+export const metadata: Metadata = {
+  title: 'Postar | Minha Conta',
+};
+
+export default async function PostarPage() {
+  return <ContaPhotoPost />;
 }
