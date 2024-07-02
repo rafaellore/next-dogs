91a86fc1c922d209d07ec00b77854a9fcad0e798
rafaellore <birdneon3@gmail.com>
2024-07-02 12:09:42 -0300

    feat(Feed): adds infinite scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@ import photosGet from '@/actions/PhotosGet';
 import Feed from '@/components/feed/Feed';
 
 export default async function Home() {
-  const data = await photosGet();
+  const { data } = await photosGet();
 
   return (
     <section className="container mainContainer">
-      {data?.ok && <Feed photos={data.data} />}
+      {data && <Feed photos={data} />}
     </section>
   );
 }
