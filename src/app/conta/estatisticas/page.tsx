273e04aab12eba23d783e3b1111490e2585d8906
rafaellore <birdneon3@gmail.com>
2024-07-15 12:48:33 -0300

    feat(ContaEstatisticas): adds graphics infos on /conta

--- src/app/conta/estatisticas/page.tsx
+++ src/app/conta/estatisticas/page.tsx
@@ -1,5 +1,26 @@
+import statsGet from '@/actions/statsGet';
+import { Metadata } from 'next';
+
+import dynamic from 'next/dynamic';
+const ContaEstatisticas = dynamic(
+  () => import('@/components/conta/ContaEstatisticas'),
+  {
+    loading: () => <p>Carregando...</p>,
+    ssr: false,
+  },
+);
+
+export const metadata: Metadata = {
+  title: 'Estatísticas | Minha Conta',
+};
+
 export default async function EstatisticasPage() {
+  const { data } = await statsGet();
+
+  if (!data) return null;
   return (
-    <div>ContaPage</div>
-  )
+    <section>
+      <ContaEstatisticas data={data} />
+    </section>
+  );
 }
